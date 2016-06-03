f43219b216c4012b0eeb959871971a85a618525b
1019120005 <1019120005@qq.com>
2016-06-03 10:06:23 +0800

    this is my first project

---
+++ src/scripts/app.js
@@ -0,0 +1,4 @@
+var word=require('app')();
+console.log(word+'1603');
+console.log(word.name);
+console.log(word.say());
---
+++ src/scripts/common.js
@@ -0,0 +1,9 @@
+
+var add={
+	"myname":"hanqing",
+	"say":"funtion(){
+		console.log(this.name);
+	}",
+	
+}
+moudule.exports(add);
